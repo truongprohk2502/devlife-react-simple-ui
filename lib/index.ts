f294c04bab88d742735de5ddbf7b5d662a913286
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2024-10-24 13:35:05 +0700

    RELEASE (serif) : new release v1.0.18

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,9 @@ import Accordion from "./components/Accordion";
 import AccordionItem from "./components/Accordion/AccordionItem";
 import Autocomplete from "./components/Autocomplete";
 import Avatar from "./components/Avatar";
+import Badge from "./components/Badge";
 import Breadcrumb from "./components/Breadcrumb";
+import BrowserMockup from "./components/BrowserMockup";
 import Button from "./components/Button";
 import Checkbox from "./components/Checkbox";
 import Chip from "./components/Chip";
@@ -12,7 +14,9 @@ import DatePicker from "./components/DatePicker";
 import Dialog from "./components/Dialog";
 import Drawer from "./components/Drawer";
 import Dropdown from "./components/Dropdown";
+import ImageDiff from "./components/ImageDiff";
 import Input from "./components/Input";
+import MobileMockup from "./components/MobileMockup";
 import Pagination from "./components/Pagination";
 import Progress from "./components/Progress";
 import Radio from "./components/Radio";
@@ -20,6 +24,8 @@ import Skeleton from "./components/Skeleton";
 import Slider from "./components/Slider";
 import Spinner from "./components/Spinner";
 import Switch from "./components/Switch";
+import Table from "./components/Table";
+import Tabs from "./components/Tabs";
 import Textarea from "./components/Textarea";
 import Toast from "./components/Toast";
 import { useToast } from "./components/Toast/useToast";
@@ -30,7 +36,9 @@ export {
   AccordionItem,
   Autocomplete,
   Avatar,
+  Badge,
   Breadcrumb,
+  BrowserMockup,
   Button,
   Checkbox,
   Chip,
@@ -39,7 +47,9 @@ export {
   Dialog,
   Drawer,
   Dropdown,
+  ImageDiff,
   Input,
+  MobileMockup,
   Pagination,
   Progress,
   Radio,
@@ -47,6 +57,8 @@ export {
   Slider,
   Spinner,
   Switch,
+  Table,
+  Tabs,
   Textarea,
   Toast,
   Tooltip,
